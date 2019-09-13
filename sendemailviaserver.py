c58fb0978e01fd45937b5b9c08c8e9fc339fd485
Chidinma Ejiofor <35844089+Zejima@users.noreply.github.com>
2019-09-12 20:02:15 -0400

    Delete sendemailviaserver.py

--- sendemailviaserver.py
+++
@@ -1,7 +0,0 @@
-import smtplib
-
-s = smtplib.SMTP('email-smtp.us-east-1.amazonaws.com',587)
-s.starttls()
-s.login('AKIAVUSM25BOEJIC7MR4','BOK++0tDpJbzJ/S7uldPzxdPnwoNFBj/mF3447qfSsec')
-msg= 'From: cretube53@gmail.com\nTo: cpejiofor@gmail.com\nSubject: Test email\n\n This is a test message'
-s.sendmail('cretube53@gmail.com','cpejiofor@gmail.com', msg)
